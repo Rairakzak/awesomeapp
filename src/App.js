d96da122f23ba8a8af8cc08d6476692df1f7934f
Rakan Douli <rairakzak@gmail.com>
2021-05-02 05:37:50 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Rakan Douli</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad cumque
+        quae, magnam voluptatum vitae distinctio at maiores corrupti incidunt
+        itaque officia beatae.
+      </p>
+    </>
+  );
+}
+
+export default App;
